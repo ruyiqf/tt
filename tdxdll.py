1456ee10f230bf269485758e7df93d797242ad98
oldwain <oldwain@163.com>
2016-10-31 17:08:41 +0800

    add buy/sell/entrust/cancel... method fix some bugs.

--- tdxdll.py
+++ tdxdll.py
@@ -1,12 +1,11 @@
-import os
 from ctypes import *
+import ctypes.util as ctypesutil
 
 
 class DLLError(Exception):
     def __init__(self, result=None):
         super(DLLError, self).__init__()
         self.result = result
-
 
 # //开发文档
 # //
@@ -15,10 +14,12 @@ class DLLError(Exception):
 # //基本版的9个函数：
 # // void  OpenTdx();//打开通达信
 # // void  CloseTdx();//关闭通达信
-# //  int  Logon(char* IP, short Port, char* Version, short YybID, char* AccountNo,char* TradeAccount, char* JyPassword,   char* TxPassword, char* ErrInfo);//登录帐号
+# //  int  Logon(char* IP, short Port, char* Version, short YybID, char* AccountNo,char* TradeAccount, char* JyPassword,
+# //             char* TxPassword, char* ErrInfo);//登录帐号
 # // void  Logoff(int ClientID);//注销
 # // void  QueryData(int ClientID, int Category, char* Result, char* ErrInfo);//查询各类交易数据
-# // void  SendOrder(int ClientID, int Category ,int PriceType,  char* Gddm,  char* Zqdm , float Price, int Quantity,  char* Result, char* ErrInfo);//下单
+# // void  SendOrder(int ClientID, int Category ,int PriceType,  char* Gddm,  char* Zqdm , float Price, int Quantity,
+# //                 char* Result, char* ErrInfo);//下单
 # // void  CancelOrder(int ClientID, char* ExchangeID, char* hth, char* Result, char* ErrInfo);//撤单
 # // void  GetQuote(int ClientID, char* Zqdm, char* Result, char* ErrInfo);//获取五档报价
 # // void  Repay(int ClientID, char* Amount, char* Result, char* ErrInfo);//融资融券账户直接还款
@@ -36,19 +37,63 @@ class DLLError(Exception):
 #
 #
 # //2.API使用流程为: 应用程序先调用OpenTdx打开通达信实例，一个实例下可以同时登录多个交易账户，每个交易账户称之为ClientID.
-# //通过调用Logon获得ClientID，然后可以调用其他API函数向各个ClientID进行查询或下单; 应用程序退出时应调用Logoff注销ClientID, 最后调用CloseTdx关闭通达信实例.
+# //通过调用Logon获得ClientID，然后可以调用其他API函数向各个ClientID进行查询或下单; 应用程序退出时应调用Logoff注销ClientID,
+# //最后调用CloseTdx关闭通达信实例.
 # //OpenTdx和CloseTdx在整个应用程序中只能被调用一次.API带有断线自动重连功能，应用程序只需根据API函数返回的出错信息进行适当错误处理即可。
+
+# 表示查询信息的种类，0资金  1股份   2当日委托  3当日成交     4可撤单   5股东代码  6融资余额   7融券余额  8可融证券
+QueryType = {
+    "balance": 0,
+    "positions": 1,
+    "today_entrust": 2,
+    "today_deal": 3,
+    "cancancel": 4,
+    "gddm": 5,
+    "rzye": 6,
+    "rqye": 7,
+    "krzq": 8
+}
+# 表示委托的种类，0买入 1卖出  2融资买入  3融券卖出   4买券还券   5卖券还款  6现券还券
+# todo: 两融的名字不会翻译， 先拼音放着了。
+OrderType = {
+    "buy": 0,
+    "sell": 1,
+    "rzbuy": 2,
+    "rqsell": 3,
+    "mqhq": 4,
+    "mqhk": 5,
+    "xqhq": 6
+}
+# 报价方式 0上海限价委托 深圳限价委托 1(市价委托)深圳对方最优价格  2(市价委托)深圳本方最优价格  3(市价委托)深圳即时成交剩余撤销
+# 4(市价委托)上海五档即成剩撤 深圳五档即成剩撤 5(市价委托)深圳全额成交或撤销 6(市价委托)上海五档即成转限价
+
+PriceType = {
+    "limit": 0,
+    "szmarket_opponent_best": 1,
+    "szmarket_self_best": 2,
+    "szmarket_commit_to_cancel": 3,
+    "market_level5_commit_to_cancel": 4,
+    "szmarket_all_commit_or_cancel": 5,
+    "shmarket_level5_commit_to_limit": 6
+}
 
 
 class TDXDLL:
     def __init__(self):
         self.sessionid = -1
-        dllpath = os.path.join(os.path.dirname(__file__), 'trader.dll')
-        #print(__file__)
-        #print(dllpath)
-        print(util.find_library('trader.dll'))
+        # dllpath = os.path.join(os.path.dirname(__file__), 'trader.dll')
+        print(ctypesutil.find_library('trader.dll'))
         self.dll = windll.LoadLibrary('trader.dll')
         self.opentdx()
+
+        self.errmsg = create_string_buffer(256)
+        self.result = create_string_buffer(1024 * 1024)
+
+    def get_last_errmsg(self):
+        return self.errmsg.value.decode('gbk')
+
+    def get_last_result(self):
+        return self.result.value.decode('gbk')
 
     def opentdx(self):
         # /// <summary>
@@ -73,13 +118,14 @@ class TDXDLL:
         # /// <param name="Version">设置通达信客户端的版本号</param>
         # /// <param name="YybID">营业部代码，请到网址 http://www.chaoguwaigua.com/downloads/qszl.htm 查询</param>
         # /// <param name="AccountNo">完整的登录账号，券商一般使用资金帐户或客户号</param>
-        # /// <param name="TradeAccount">交易账号，一般与登录帐号相同. 请登录券商通达信软件，查询股东列表，股东列表内的资金帐号就是交易帐号, 具体查询方法请见网站“热点问答”栏目</param>
+        # /// <param name="TradeAccount">交易账号，一般与登录帐号相同. 请登录券商通达信软件，查询股东列表，
+        # ///                            股东列表内的资金帐号就是交易帐号, 具体查询方法请见网站“热点问答”栏目</param>
         # /// <param name="JyPassword">交易密码</param>
         # /// <param name="TxPassword">通讯密码</param>
         # /// <param name="ErrInfo">此API执行返回后，如果出错，保存了错误信息说明。一般要分配256字节的空间。没出错时为空字符串。</param>
         # /// <returns>客户端ID，失败时返回-1</returns>
-        # ///  int  Logon(char* IP, short Port, char* Version,short YybID,  char* AccountNo,char* TradeAccount, char* JyPassword,   char* TxPassword, char* ErrInfo);
-        errmsg = create_string_buffer(256)
+        # ///  int  Logon(char* IP, short Port, char* Version,short YybID,  char* AccountNo,char* TradeAccount,
+        # ///             char* JyPassword,   char* TxPassword, char* ErrInfo);
         sessionid = self.dll.Logon(ip.encode(),
                                    port,
                                    b'6.50',
@@ -88,10 +134,10 @@ class TDXDLL:
                                    account.encode(),
                                    pwd.encode(),
                                    b'',
-                                   errmsg
+                                   self.errmsg
                                    )  # 登录帐号
         if sessionid == -1:
-            raise DLLError(errmsg.value)
+            raise DLLError(self.errmsg.value)
         self.sessionid = sessionid
         return True
 
@@ -108,14 +154,15 @@ class TDXDLL:
         # /// 查询各种交易数据
         # /// </summary>
         # /// <param name="ClientID">客户端ID</param>
-        # /// <param name="Category">表示查询信息的种类，0资金  1股份   2当日委托  3当日成交     4可撤单   5股东代码  6融资余额   7融券余额  8可融证券</param>
-        # /// <param name="Result">此API执行返回后，Result内保存了返回的查询数据, 形式为表格数据，行数据之间通过\n字符分割，列数据之间通过\t分隔。一般要分配1024*1024字节的空间。出错时为空字符串。</param>
+        # /// <param name="Category">表示查询信息的种类，0资金  1股份   2当日委托  3当日成交 4可撤单
+        # ///                        5股东代码  6融资余额   7融券余额  8可融证券</param>
+        # /// <param name="Result">此API执行返回后，Result内保存了返回的查询数据, 形式为表格数据，
+        # ///                      行数据之间通过\n字符分割，列数据之间通过\t分隔。
+        # ///                      一般要分配1024*1024字节的空间。出错时为空字符串。</param>
         # /// <param name="ErrInfo">同Logon函数的ErrInfo说明</param>
 
-        errmsg = create_string_buffer(256)
-        result = create_string_buffer(1024 * 1024)
-        self.dll.QueryData(self.sessionid, category, result, errmsg)
-        return (True, result.value.decode('gbk')) if errmsg.value == b"" else (False, errmsg.value.decode('gbk'))
+        self.dll.QueryData(self.sessionid, category, self.result, self.errmsg)
+        return (True, self.get_last_result()) if self.errmsg.value == b"" else (False, self.get_last_errmsg())
 
     def sendorder(self, category, pricetype, gddm, zqdm, price, quantity):
         # /// <summary>
@@ -123,17 +170,33 @@ class TDXDLL:
         # /// </summary>
         # /// <param name="ClientID">客户端ID</param>
         # /// <param name="Category">表示委托的种类，0买入 1卖出  2融资买入  3融券卖出   4买券还券   5卖券还款  6现券还券</param>
-        # /// <param name="PriceType">表示报价方式 0上海限价委托 深圳限价委托 1(市价委托)深圳对方最优价格  2(市价委托)深圳本方最优价格  3(市价委托)深圳即时成交剩余撤销  4(市价委托)上海五档即成剩撤 深圳五档即成剩撤 5(市价委托)深圳全额成交或撤销 6(市价委托)上海五档即成转限价
+        # /// <param name="PriceType">表示报价方式 0上海限价委托 深圳限价委托 1(市价委托)深圳对方最优价格
+        # ///                         2(市价委托)深圳本方最优价格  3(市价委托)深圳即时成交剩余撤销
+        # ///                         4(市价委托)上海五档即成剩撤 深圳五档即成剩撤 5(市价委托)深圳全额成交或撤销
+        # ///                         6(市价委托)上海五档即成转限价
         # /// <param name="Gddm">股东代码, 交易上海股票填上海的股东代码；交易深圳的股票填入深圳的股东代码</param>
         # /// <param name="Zqdm">证券代码</param>
         # /// <param name="Price">委托价格</param>
         # /// <param name="Quantity">委托数量</param>
         # /// <param name="Result">同上,其中含有委托编号数据</param>
         # /// <param name="ErrInfo">同上</param>
-        # /// void  SendOrder(int ClientID, int Category ,int PriceType,  char* Gddm,  char* Zqdm , float Price, int Quantity,  char* Result, char* ErrInfo);
+        # /// void  SendOrder(int ClientID, int Category ,int PriceType,  char* Gddm,  char* Zqdm , float Price,
+        # ///                 int Quantity,  char* Result, char* ErrInfo);
 
-        errmsg = create_string_buffer(256)
-        result = create_string_buffer(1024 * 1024)
-        self.dll.SendOrder(self.sessionid, category, pricetype, gddm.encode(), zqdm.encode(), price, quantity, result,
-                           errmsg)
-        return (True, result.value.decode('gbk')) if errmsg.value == b"" else (False, errmsg.value.decode('gbk'))
+        self.dll.SendOrder(self.sessionid, category, pricetype, gddm.encode(), zqdm.encode(),
+                           c_float(price), quantity, self.result, self.errmsg)
+        return (True, self.get_last_result()) if self.errmsg.value == b"" else (False, self.get_last_errmsg())
+
+    def cancelorder(self, exchangeid, wth):
+        # /// <summary>
+        # /// 撤委托
+        # /// </summary>
+        # /// <param name="ClientID">客户端ID</param>
+        # /// <param name="ExchangeID">交易所ID， 上海1，深圳0(招商证券普通账户深圳是2)</param>
+        # /// <param name="hth">表示要撤的目标委托的编号</param>
+        # /// <param name="Result">同上</param>
+        # /// <param name="ErrInfo">同上</param>
+        # /// void  CancelOrder(int ClientID, char* ExchangeID, char* hth, char* Result, char* ErrInfo);
+        self.dll.CancelOrder(self.sessionid, exchangeid.encode(), wth.encode(),self.result, self.errmsg)
+
+        return (True, self.get_last_result()) if self.errmsg.value == b"" else (False, self.get_last_errmsg())
--- tdxtrader.py
+++ tdxtrader.py
@@ -1,35 +1,11 @@
 # coding: utf-8
 import os
 import time
-
-from .tdxdll import DLLError
-from .tdxdll import TDXDLL
+import re
+from .tdxdll import DLLError, TDXDLL, QueryType, OrderType, PriceType
 from . import helpers
 from .log import log
 
-# 表示查询信息的种类，0资金  1股份   2当日委托  3当日成交     4可撤单   5股东代码  6融资余额   7融券余额  8可融证券
-QueryType = {
-    "balance": 0,
-    "positions": 1,
-    "today_entrust": 2,
-    "today_commit": 3,
-    "cancancel": 4,
-    "gddm": 5,
-    "rzye": 6,
-    "rqye": 7,
-    "krzq": 8
-}
-OrderTYpe = {
-#表示委托的种类，0买入 1卖出  2融资买入  3融券卖出   4买券还券   5卖券还款  6现券还券
-# todo: 两融的名字不会翻译， 先拼音放着了。
-    "buy": 0,
-    "sell": 1,
-    "rzbuy": 2,
-    "rqsell": 3,
-    "mqhq": 4,
-    "mqhk": 5,
-    "xqhq": 6
-}
 
 class NotLoginError(Exception):
     def __init__(self, result=None):
@@ -50,6 +26,17 @@ class TDXTrader:
         self.dll = TDXDLL()
 
         self.exchange_stock_account = dict()
+
+    # #####################################################################
+    # 初始化、登录类方法
+    # #####################################################################
+
+    def prepare(self, need_data):
+        """登录的统一接口
+        :param need_data 登录所需数据
+        """
+        self.read_config(need_data)
+        self.autologin()
 
     def login(self, throw=False):
         broker = self.account_config['broker']
@@ -75,13 +62,6 @@ class TDXTrader:
     def logoff(self):
         self.dll.logoff()
 
-    def prepare(self, need_data):
-        """登录的统一接口
-        :param need_data 登录所需数据
-        """
-        self.read_config(need_data)
-        self.autologin()
-
     def autologin(self, limit=3):
         """实现自动登录
         :param limit: 登录次数限制
@@ -97,12 +77,14 @@ class TDXTrader:
             raise NotLoginError('登录失败次数过多, 请检查密码是否正确 / 券商服务器是否处于维护中 / 网络连接是否正常')
         # self.keepalive()  # 接口本身似乎带心跳， 先观察
 
-
+    # #####################################################################
+    # 查询类方法
+    # #####################################################################
 
     def _query(self, querytype):
         success, data = self.dll.querydata(querytype)
         if success:
-            return format_result(data)
+            return self.format_response_data(data)
         else:
             log.info("error: " + data)
             return ""
@@ -131,17 +113,10 @@ class TDXTrader:
         """获取当日委托列表"""
         return self._query(QueryType["today_entrust"])
 
-    @property
-    def commits(self):
-        return self.get_entrust()
-
-    def get_commits(self):
-        """获取当日委托列表"""
-        return self._query(QueryType["today_commit"])
-
     def get_can_cancel(self):
         """获取当日委托列表"""
         return self._query(QueryType["cancancel"])
+
     def get_share_holder_account(self):
         data = self._query(QueryType["gddm"])
 
@@ -149,42 +124,73 @@ class TDXTrader:
             self.exchange_stock_account[d['帐号类别']] = d['股东代码']
 
         return self.exchange_stock_account
+
     def check_available_cancels(self):
-        pass
-
-    def cancel_entrust(self, entrust_no, stock_code):
-        pass
-
-    def cancel_entrusts(self, entrust_no):
-        pass
+        return self.get_can_cancel()
 
     @property
     def current_deal(self):
         return self.get_current_deal()
 
     def get_current_deal(self):
-        pass
+        return self._query(QueryType["today_deal"])
 
-    def buy(self, stock_code, price, amount=0, volume=0, entrust_prop=0):
-        pass
+    # #####################################################################
+    # 操作类方法
+    # #####################################################################
+
+    def _do(self, ordertype, pricetype, stock_code, price, quantity):
+        sh_exchange_type = '1'
+        sz_exchange_type = '0'
+        exchange_type = sh_exchange_type if helpers.get_stock_type(stock_code) == 'sh' else sz_exchange_type
+        gddm = self.exchange_stock_account[exchange_type]
+
+        success, data = self.dll.sendorder(ordertype, pricetype, gddm, stock_code, price, quantity)
+
+        if success:
+            return self.format_response_data(data)
+        else:
+            log.info("error: " + data)
+            return ""
+
+    def buy(self, stock_code, price, amount=0., volume=0, entrust_prop=0):
+        vol = volume if volume > 0 else (amount/price // 100) * 100
+        pricetype = PriceType(entrust_prop) if type(entrust_prop) == "str" else entrust_prop
+        return self._do(OrderType["buy"], pricetype, stock_code, price, vol)
 
     def sell(self, stock_code, price, amount=0, volume=0, entrust_prop=0):
+        vol = volume if volume > 0 else (amount/price // 100) * 100
+        pricetype = PriceType(entrust_prop) if type(entrust_prop) == "str" else entrust_prop
+        return self._do(OrderType["sell"], pricetype, stock_code, price, vol)
+
+    def cancel_entrusts(self, entrust_no):
+        entrusts = self.get_can_cancel()
+        for e in entrusts:
+            if e['委托编号'] == entrust_no:
+                exchangeid = e['交易所代码']
+                success, data = self.dll.cancelorder(exchangeid, entrust_no)
+                return success
+        return False
+
+    def cancel_entrust(self, entrust_no, stock_code):
         pass
 
-    def __trade(self, stock_code, price, entrust_prop, other):
-        pass
+    # #####################################################################
+    # 其它查询类方法
+    # #####################################################################
 
-    def __get_trade_need_info(self, stock_code):
-        pass
+    # todo: 历史查询 0历史委托  1历史成交   2交割单 (做的必要行不大？)
 
-    def format_response_data(self, data):
-        pass
+    # todo: 查询某股票的实时5档行情(似乎有用， 感觉券商行情比用网络免费行情源要可靠， 可以替代easyquotation)
 
-    def check_account_live(self, response):
-        pass
+    def get_last_errmsg(self):
+        return self.dll.get_last_errmsg()
 
-    def heartbeat(self):
-        pass
+    def get_last_result(self):
+        return self.dll.get_last_result()
+    # #####################################################################
+    # 辅助类方法
+    # #####################################################################
 
     def __read_config(self):
         """读取 config"""
@@ -201,21 +207,44 @@ class TDXTrader:
             if type(v) is int:
                 log.warn('配置文件的值最好使用双引号包裹，使用字符串类型，否则可能导致不可知的问题')
 
+    def format_response_data_type(self, response_data):
+        """格式化返回的值为正确的类型
+        :param response_data: 返回的数据
+        """
+        if type(response_data) is not list:
+            return response_data
 
-def format_result(data):
+        int_match_str = '|'.join(self.config['response_format']['int'])
+        float_match_str = '|'.join(self.config['response_format']['float'])
+        for item in response_data:
+            for key in item:
+                try:
+                    if re.search(int_match_str, key) is not None:
+                        item[key] = helpers.str2num(item[key], 'int')
+                    elif re.search(float_match_str, key) is not None:
+                        item[key] = helpers.str2num(item[key], 'float')
+                except ValueError:
+                    continue
+        return response_data
 
-    lines = data.split("\n")
-    head = True
-    body = []
-    for line in lines:
-        if head:
-            heads = line.split("\t")
-        else:
-            fields = line.split("\t")
-            body.append(dict(zip(heads, fields)))
-        head = False
-    #print (heads)
-    #print(body)
-    return body
+    def format_response_data(self, data):
+
+        lines = data.split("\n")
+        field_heads = []
+        body = []
+        firstline = True
+        for line in lines:
+            if firstline:
+                field_heads = line.split("\t")
+                firstline = False
+            else:
+                fields = line.split("\t")
+                body.append(dict(zip(field_heads, fields)))
+        return self.format_response_data_type(body)
+
+# #####################################################################
+# 辅助类函数
+# #####################################################################
+
 
 
